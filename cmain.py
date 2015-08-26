94d5939684a03c8f5daeef5b1fdaca1cf9a0346e
Dubslow <bunslow@gmail.com>
2015-08-26 03:37:30 -0500

    Add C parsing. Currently limited by the strictness of pycparser

---
+++ cmain.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+from codeschematics.parsers.c_parser import make_call_dict
+from codeschematics.presentation import Presenter
+
+from sys import argv
+from os.path import basename
+
+################################################################################
+
+fname = argv[1]
+
+dic, nested = make_call_dict(fname) # Ignore the nested funcs retval
+
+tree = Presenter(dic)
+
+fname = basename(fname)
+tree = tree.default_filter()
+tree.to_svg(fname)
---
+++ codeschematics/parsers/c_parser.py
@@ -0,0 +1,61 @@
+# Note: tab depth is 5, as a personal preference
+
+
+#    Copyright (C) 2014-2015 Bill Winslow
+#
+#    This module is a part of the CodeSchematics package.
+#
+#    This program is libre software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#    See the LICENSE file for more details.
+
+
+# The second parser for my "parse-a-program-and-diagram-its-design" lib
+
+'''This module uses the pycparser package to parse a given C file and
+produce a list of defined functions and their subcalls suitable for passing
+to the codeschematics.presentation module.'''
+
+from __future__ import print_function
+from pycparser import c_ast, parse_file
+from codeschematics.parsers.parser_data import ParserData
+
+class CParser(c_ast.NodeVisitor):
+
+     top_level = '__file__' # The fake name for containing-function of 
+                            # top level function calls
+     def __init__(self):
+          self._data = ParserData(self.top_level)
+
+     def visit_FuncDef(self, node):
+          # For now, we assume that function name is unique (i.e. no overloading),
+          # so ignore param types and return type
+          self._data.parse_func(node.decl.name, self.generic_visit, node)
+
+     def visit_FuncCall(self, node):
+          self._data.func_called(node.name.name)            
+          self.generic_visit(node)
+
+     def result(self):
+          '''After parsing is complete, call this to get the function->subcalls
+          dictionary and nested functions set (as a tuple).'''
+          return self._data.result()
+
+
+def make_call_dict(filename):
+     '''This parses the given file into an AST, then traverses the AST to create
+     the function definition list. The return value is a tuple of
+     (function_def_dict, set_of_nested_funcs), where the latter is the set of
+     functions that aren't defined at top level in the module.'''
+     tree = parse_file(filename, use_cpp=True)
+     parser = CParser()
+     #print('starting traversal')
+     parser.visit(tree)
+     return parser.result()
